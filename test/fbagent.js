5935b47451357de36558bece17ea859e556d8c2c
Jake Luer <jake.luer@gmail.com>
2012-07-04 19:42:27 -0700

    tests for posts/deletes

--- test/fbagent.js
+++ test/fbagent.js
@@ -1,10 +1,8 @@
 var chai = require('chai')
-  , should = chai.should()
-  , chaihttp = require('chai-http');
+  , should = chai.should();
 
-chai.use(chaihttp);
-
-var fbagent = require('..');
+var fbagent = require('..')
+  , config = require('./config.json');
 
 describe('fbagent', function () {
 
@@ -13,12 +11,68 @@ describe('fbagent', function () {
   });
 
   describe('untokened get requests', function () {
-    it('should work', function (done) {
+
+    it('can get a user', function (done) {
       fbagent
         .get('/jakeluer')
         .end(function (err, res) {
           should.not.exist(err);
           res.should.have.property('username', 'jakeluer');
+          done();
+        });
+    });
+
+  });
+
+  describe('tokened request', function () {
+    var testUser;
+
+    it('can create a test user', function (done) {
+      fbagent
+        .get(config.appId + '/accounts/test-users')
+        .token(config.appId + '|' + config.appSecret)
+        .send({
+            installed: true
+          , name: 'Rory Williams'
+          , permissions: config.scope
+          , method: 'post'
+        })
+        .end(function (err, res) {
+          should.not.exist(err);
+          testUser = res;
+          testUser.should.include.keys('id', 'access_token', 'email', 'password');
+          done();
+        });
+    });
+
+    it('can get a protected resource', function (done) {
+      fbagent
+        .get('/me')
+        .token(testUser.access_token)
+        .end(function (err, res) {
+          should.not.exist(err);
+          res.should.include.keys('id', 'name', 'email');
+          done();
+        });
+    });
+
+    it('can post to a protected resource', function (done) {
+      fbagent
+        .post('/me/feed')
+        .token(testUser.access_token)
+        .send({ message: 'If only I could remember where i parked my tardis.' })
+        .end(function (err, res) {
+          should.not.exist(err);
+          done();
+        });
+    });
+
+    it('can delete a protected resource', function (done) {
+      fbagent
+        .del('/' + testUser.id)
+        .token(config.appId + '|' + config.appSecret)
+        .end(function (err, res) {
+          should.not.exist(err);
           done();
         });
     });
